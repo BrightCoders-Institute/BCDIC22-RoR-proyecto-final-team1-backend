9e63605375328010d5ffc1ac2bd0e7ed37305e99
Severo Iglesias <siglesias@ucol.mx>
2023-03-24 12:48:08 -0600

    add controller for the api crud amenities

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'amenities/index'
+  get 'amenities/show'
+  get 'amenities/create'
+  get 'amenities/update'
+  get 'amenities/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
