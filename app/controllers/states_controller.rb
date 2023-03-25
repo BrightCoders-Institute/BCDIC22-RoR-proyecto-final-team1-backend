47d26b537c56c43d9389aa746270545fb8b89420
yaxkin121 <ycardenas13@ucol.mx>
2023-03-24 18:18:11 -0600

    Add methods for state
    Co-authored-by: Reiyirak <pdsmej@gmail.com>

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,16 +1,33 @@
 class StatesController < ApplicationController
   def index
+
   end
 
   def show
   end
 
   def create
+    state = State.new(
+			name: state_params[:name]
+		)
+		if state.save
+			render json: state, status: 200
+		else
+			render json: {
+				error: "error creating.."
+			}
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def state_params
+    params.require(:state).permit([:name])
   end
 end
