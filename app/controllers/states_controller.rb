154a49b29ad14b66263716a66be0bd5e8b8a54e4
yaxkin121 <ycardenas13@ucol.mx>
2023-03-24 18:25:04 -0600

    Add method post and get for states
    Co-authored-by: Reiyirak <pdsmej@gmail.com>

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,9 +1,18 @@
 class StatesController < ApplicationController
   def index
-
+    states = State.all
+		render json: states, status: 200
   end
 
   def show
+    state = State.find_by(id: params[:id])
+		if state
+			render json: state, status: 200
+		else
+			render json: {
+				error: "Article not found"
+			}
+		end
   end
 
   def create
