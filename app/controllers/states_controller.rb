cda497c126f174a2fd2304526116f4dacfd98d20
yaxkin121 <ycardenas13@ucol.mx>
2023-03-24 18:38:06 -0600

    Final commit
    Co-authored-by: Reiyirak <pdsmej@gmail.com>

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,37 +1,51 @@
 class StatesController < ApplicationController
   def index
     states = State.all
-		render json: states, status: 200
+    render json: states, status: 200
   end
 
   def show
     state = State.find_by(id: params[:id])
-		if state
-			render json: state, status: 200
-		else
-			render json: {
-				error: "Article not found"
-			}
-		end
+    if state
+      render json: state, status: 200
+    else
+      render json: {
+        error: 'State not found'
+      }
+    end
   end
 
   def create
     state = State.new(
-			name: state_params[:name]
-		)
-		if state.save
-			render json: state, status: 200
-		else
-			render json: {
-				error: "error creating.."
-			}
+      name: state_params[:name]
+    )
+    if state.save
+      render json: state, status: 200
+    else
+      render json: {
+        error: 'error creating..'
+      }
     end
   end
 
   def update
+    state = State.find_by(id: params[:id])
+    if state
+      state.update(name: params[:name])
+      render json: 'State record update successfully'
+    else
+      render json: {
+        error: 'State Not Found'
+      }
+    end
   end
 
   def destroy
+    state = State.find_by(id: params[:id])
+    return unless state
+
+    state.destroy
+    render json: 'State has been deleted'
   end
 
   private
