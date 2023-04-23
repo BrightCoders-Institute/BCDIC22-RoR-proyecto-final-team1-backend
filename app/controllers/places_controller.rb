1996b9faafadb67539fa1b737232048ad4b6f18f
Reiyirak <pdsmej@gmail.com>
2023-04-22 19:42:52 -0600

    Modify places controller
    I modified the places controller so that we can get all information of
    the user on the frontend if we need it.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,7 +7,8 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find_by(id: params[:id])
     if @place
-      render json: { user: @place.user.name, name: @place.name, description: @place.description, max_guest: @place.max_guest, number_rooms: @place.number_rooms, number_bathrooms: @place.number_bathrooms, latitude: @place.latitude, longitude: @place.longitude, city: @place.city.name, state: @place.city.state.name, price_by_night: @place.price_by_night }
+      render json: { user: @place.user, name: @place.name, description: @place.description,
+                     max_guest: @place.max_guest, number_rooms: @place.number_rooms, number_bathrooms: @place.number_bathrooms, latitude: @place.latitude, longitude: @place.longitude, city: @place.city.name, state: @place.city.state.name, price_by_night: @place.price_by_night }
     else
       render json: { error: 'Place not found' }, status: :not_found
     end
@@ -19,16 +20,15 @@ class PlacesController < ApplicationController
 
   def create
     @place = Place.new(place_params)
-    if @place.valid? 
+    if @place.valid?
       @place.save
-      render json: { place: @place, message: "Place created successfully" }, status: :created
+      render json: { place: @place, message: 'Place created successfully' }, status: :created
     else
       render json: { errors: @place.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @place = Place.find(params[:id])
@@ -57,6 +57,7 @@ class PlacesController < ApplicationController
   private
 
   def place_params
-    params.require(:place).permit(:user_id, :name, :city_id, :description, :number_rooms, :number_bathrooms, :max_guest, :price_by_night, :latitude, :longitude)
+    params.require(:place).permit(:user_id, :name, :city_id, :description, :number_rooms, :number_bathrooms,
+                                  :max_guest, :price_by_night, :latitude, :longitude)
   end
 end
