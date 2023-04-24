8dd61fe23566f02e3e7a4f0db20e9d901abc1a69
Reiyirak <pdsmej@gmail.com>
2023-04-24 01:34:34 -0600

    Change token expiration time to a week

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -338,6 +338,6 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 30.minutes.to_i
+    jwt.expiration_time = 10080.minutes.to_i
   end
 end
